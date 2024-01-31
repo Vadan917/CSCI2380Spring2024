1eb669e232ef33cfc52a253aea9ae017d3b30686
Vadan917 <valnor12@hotmail.com>
2024-01-31 02:24:10 -0600

    Add files via upload

---
+++ TicTacToe/tictactoe.cpp
@@ -0,0 +1,33 @@
+// TO DO: Write your name and your teammate name
+
+#include "tictactoe.h"
+#include <iostream>
+
+using namespace std;
+
+tictactoe::tictactoe()
+{
+	// ************* TO DO*******************
+
+}
+
+
+bool tictactoe::PlaceMove(int x, int y)
+{
+	// ************* TO DO*******************
+}
+
+void tictactoe::DrawBoard()
+{
+	// ************* TO DO*******************
+}
+
+bool tictactoe::GameOver()
+{
+	// ************* TO DO*******************
+}
+
+char tictactoe::Winner()
+{
+	// ************* TO DO*******************
+}
